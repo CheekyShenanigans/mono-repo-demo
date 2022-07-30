ad890f8f2ef4702a0e6f1aa59f0baa6da20e12d2
Aaron Ward <aaron.l.ward@gmail.com>
2022-07-30 02:08:42 -0500

    working example of host and remote using webpack 5 module federation

---
+++ README.md
@@ -0,0 +1,19 @@
+# Overview
+Demonstrate webpack 5 module federation
+
+## Getting Started
+
+``` bash
+# run the remote
+cd ../remote
+npm start
+
+# also, run the host
+cd ../host
+npm start
+
+# launch localhost:3000
+```
+
+## Resources
+https://betterprogramming.pub/how-to-use-webpack-module-federation-in-react-70455086b2b0
---
+++ host/webpack.config.js
@@ -0,0 +1,64 @@
+// host/webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { dependencies } = require("./package.json");
+
+const cssLoaderRule = { test: /\.css$/, use: 'css-loader' }
+const imageLoaderRule = {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    };
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      cssLoaderRule,
+      imageLoaderRule,
+      {
+
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Host",
+      remotes: {
+        Remote: `Remote@http://localhost:4000/moduleEntry.js`,
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    // moduleFederationPlugin,
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
