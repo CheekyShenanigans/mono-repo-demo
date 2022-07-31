a019c949a1e713fa4a70d81c9a13fa912b1ffd5d
Aaron Ward <aaron.l.ward@gmail.com>
2022-07-31 14:58:47 -0500

    build and deploy to github pages

---
+++ remote/webpack.prod.js
@@ -0,0 +1,45 @@
+// remote/webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "production",
+  devServer: {
+    static: './dist',
+    port: 4000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Remote",
+      filename: "moduleEntry.js",
+      exposes: {
+        "./App": "./src/App",
+        "./Button": "./src/Button",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
