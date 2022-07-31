1fea96757bc2f8914a6a6fc14766b30ec1a70dca
FreakinWard <aaron.l.ward@gmail.com>
2022-07-31 21:34:14 +0000

    Deploying to gh_pages from  @ 94414dd8a94b82dea63d0edb10c044ec4d43b3f5 🚀

---
+++ remote/283.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkremote=self.webpackChunkremote||[]).push([[283],{283:(e,t,r)=>{r.r(t);var l=r(294);function n(){return l.createElement("div",null,"Hello from the other side")}r(745).createRoot(document.getElementById("root")).render(l.createElement(l.StrictMode,null,l.createElement(n,null)))}}]);
